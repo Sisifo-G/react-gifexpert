07040022508928424fa52278357d091d2c2a6dcb
Manuel Perdomo <manuelperdomo1710@icloud.com>
2020-06-25 05:56:34 -0500

    Docs agregado

---
+++ docs/precache-manifest.0d0258e476840f36f177f0cb400ebfd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d078d707f196d473e3523a50a6bab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abe14738c9903c4e3b72",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe14738c9903c4e3b72",
+    "url": "/static/js/main.10bbf962.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
